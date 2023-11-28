4a30b67f7dff0d5def3179007fc57e24c79eca17
Anastasiia Podopryhora <anastasia95podoprigora@gmail.com>
2023-11-28 18:48:46 +0100

    test

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/project-management-server";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb+srv://anastasia95podoprigora:uNS2vmhLGAdzZaIv@finalproject.bk7p7qb.mongodb.net/";
 
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }) 
